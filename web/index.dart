a2aa38b9e9ec8f0f968173bba56df27ed0060018
Louis Laagland <louis@wunderful.nl>
2016-01-30 22:14:54 +0100

    Cleanup bootstrap
    Signed-off-by: Louis Laagland <louis@wunderful.nl>

--- web/index.dart
+++ web/index.dart
@@ -1,13 +1,9 @@
-import 'package:angular2/angular2.dart';
 import 'package:angular2/bootstrap.dart';
-import 'package:angular2/router.dart';
 import 'package:ng2_dart_pagination/app.dart';
 
 void main() {
   var app = DemoApp;
-  var bindings = [injectables,
-  bind(APP_BASE_HREF).toValue('/'),
-  bind(LocationStrategy).toClass(HashLocationStrategy)];
+  var bindings = [injectables];
 
   bootstrap(app, bindings);
 }
